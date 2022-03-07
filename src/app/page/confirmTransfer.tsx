3c8f4595d0086d880a8f9ae01dec6c5d2de57070
tuanle161099 <tuanle@descartes.network>
2022-03-07 13:44:41 +0700

    update decimal

--- src/app/page/confirmTransfer.tsx
+++ src/app/page/confirmTransfer.tsx
@@ -1,35 +1,71 @@
-import { useDispatch } from 'react-redux'
+import { ReactNode, useCallback, useEffect, useMemo, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useAccount, useWallet } from '@senhub/providers'
+import { utils } from '@senswap/sen-js'
 import moment from 'moment'
 
 import { Button, Card, Col, Row, Space, Tag, Typography } from 'antd'
 import PoweredBySentre from 'app/components/poweredBySentre'
 
-import { randomColor } from 'shared/util'
-import { AppDispatch } from 'app/model'
+import { AppDispatch, AppState } from 'app/model'
 import { onSelectStep } from 'app/model/steps.controller'
 import { Step } from 'app/constants'
+import useTotalTransfer from 'app/hooks/useTotalTransfer'
+import { MintSymbol } from 'shared/antd/mint'
+import useMintDecimals from 'shared/hooks/useMintDecimals'
+import { numeric } from 'shared/util'
 
 const Content = ({
   label = '',
   value = '',
 }: {
   label?: string
-  value?: string | number
+  value?: ReactNode
 }) => {
   return (
     <Row>
       <Col flex="auto">
-        <Typography.Text type="secondary">{label}</Typography.Text>
+        <Typography.Text type="secondary">{label} </Typography.Text>
       </Col>
-      <Col>
-        <Typography.Text>{value}</Typography.Text>
-      </Col>
+      <Col>{value}</Col>
     </Row>
   )
 }
 
 const ConfirmTransfer = () => {
+  const [balance, setBalance] = useState(0)
+  const {
+    main: { mintSelected },
+    setting: { decimal },
+  } = useSelector((state: AppState) => state)
+  const {
+    wallet: { address: walletAddress },
+  } = useWallet()
   const dispatch = useDispatch<AppDispatch>()
+  const { total, quantity } = useTotalTransfer()
+  const { accounts } = useAccount()
+  const mintDecimals = useMintDecimals(mintSelected) || 0
+
+  const getBalanceAccount = useCallback(async () => {
+    const { splt } = window.sentre
+    const accountAddress = await splt.deriveAssociatedAddress(
+      walletAddress,
+      mintSelected,
+    )
+    const { amount } = accounts[accountAddress] || {}
+    if (!amount) return setBalance(0)
+    if (decimal) return setBalance(Number(amount))
+    setBalance(Number(utils.undecimalize(amount, mintDecimals)))
+  }, [accounts, decimal, mintDecimals, mintSelected, walletAddress])
+
+  const remainingBalance = useMemo(() => {
+    if (!balance) return 0
+    return Number(balance) - total
+  }, [balance, total])
+
+  useEffect(() => {
+    getBalanceAccount()
+  }, [getBalanceAccount])
 
   return (
     <Card bordered={false}>
@@ -51,16 +87,17 @@ const ConfirmTransfer = () => {
                 <Typography.Text type="secondary">
                   Total transfer
                 </Typography.Text>
-                <Typography.Title level={2}>${18000}</Typography.Title>
+                <Typography.Title level={2}>${total}</Typography.Title>
                 <Tag
                   style={{
                     margin: 0,
                     borderRadius: 4,
-                    color: randomColor('SNTR'),
+                    color: 'rgb(249, 88, 96)',
+                    background: 'rgba(249, 88, 96, 0.1)',
+                    border: 'unset',
                   }}
-                  color={randomColor('SNTR', 0.2)}
                 >
-                  {'SNTR'}
+                  <MintSymbol mintAddress={mintSelected} />
                 </Tag>
               </Space>
             </Col>
@@ -74,13 +111,29 @@ const ConfirmTransfer = () => {
                     />
                   </Col>
                   <Col span={24}>
-                    <Content label="Quantity" value={9} />
+                    <Content label="Quantity" value={quantity} />
                   </Col>
                   <Col span={24}>
-                    <Content label="Your balance" value={'16 SNTR'} />
+                    <Content
+                      label="Your balance"
+                      value={
+                        <Typography.Text>
+                          {numeric(balance).format('0,0.00[0000]')}{' '}
+                          <MintSymbol mintAddress={mintSelected} />
+                        </Typography.Text>
+                      }
+                    />
                   </Col>
                   <Col span={24}>
-                    <Content label="Remaing" value={'6 SNTR'} />
+                    <Content
+                      label="Remaining"
+                      value={
+                        <Typography.Text>
+                          {numeric(remainingBalance).format('0,0.00[00]')}{' '}
+                          <MintSymbol mintAddress={mintSelected} />
+                        </Typography.Text>
+                      }
+                    />
                   </Col>
                 </Row>
               </Card>
@@ -90,12 +143,21 @@ const ConfirmTransfer = () => {
         <Col span={24}>
           <Row gutter={[8, 8]}>
             <Col span={12}>
-              <Button onClick={() => dispatch(onSelectStep(Step.one))} block>
+              <Button
+                size="large"
+                onClick={() => dispatch(onSelectStep(Step.one))}
+                block
+              >
                 Back
               </Button>
             </Col>
             <Col span={12}>
-              <Button type="primary" block>
+              <Button
+                size="large"
+                disabled={remainingBalance < 0}
+                type="primary"
+                block
+              >
                 Confirm
               </Button>
             </Col>

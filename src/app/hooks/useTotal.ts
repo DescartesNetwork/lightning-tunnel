e32b75d426bbd99d66f1f3487c0c3bd69db98a57
tuanle161099 <tuanle@descartes.network>
2022-03-09 00:32:54 +0700

    -

--- src/app/hooks/useTotal.ts
+++ src/app/hooks/useTotal.ts
@@ -1,12 +1,17 @@
+import util from '@senswap/sen-js/dist/utils'
 import { AppState } from 'app/model'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
+import useMintDecimals from 'shared/hooks/useMintDecimals'
 
 const useTotal = () => {
   const {
+    setting: { decimal },
+    main: { mintSelected },
     recipients: { recipients },
   } = useSelector((state: AppState) => state)
   const [total, setTotal] = useState(0)
+  const mintDecimals = useMintDecimals(mintSelected) || 0
 
   const calculateTotal = useCallback(() => {
     let sum = 0
@@ -14,8 +19,9 @@ const useTotal = () => {
       const amount = Number(item[2])
       return (sum += amount)
     })
-    setTotal(sum)
-  }, [recipients])
+    if (decimal) return setTotal(Number(util.decimalize(sum, mintDecimals)))
+    return setTotal(sum)
+  }, [decimal, mintDecimals, recipients])
 
   const quantity = useMemo(() => recipients.length, [recipients])
 

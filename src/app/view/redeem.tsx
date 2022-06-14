1bbfa68c7bc9c4845043d85eb0c6a59be1525eb9
tuanle161099 <tuanle@descartes.network>
2022-06-14 13:33:27 +0700

    update loading

--- src/app/view/redeem.tsx
+++ src/app/view/redeem.tsx
@@ -6,7 +6,7 @@ import { DistributorData, FeeOptions, MerkleDistributor } from '@sentre/utility'
 import { BN } from 'bn.js'
 import moment from 'moment'
 
-import { Image, Space, Typography, Row, Col, Button, Card } from 'antd'
+import { Image, Space, Typography, Row, Col, Button, Card, Spin } from 'antd'
 import IonIcon from '@sentre/antd-ionicon'
 import ButtonHome from 'app/components/buttonHome'
 
@@ -150,54 +150,57 @@ const Redeem = () => {
   return (
     <Row gutter={[24, 24]} justify="center" className="lightning-container">
       <Col xs={24} md={16} lg={12} xl={10}>
-        <Card
-          style={{ minHeight: 430 }}
-          loading={loadingCard}
-          className="card-lightning"
-          bordered={false}
-        >
-          <Row gutter={[32, 32]} style={{ textAlign: 'center' }}>
-            <Col span={24} style={{ textAlign: 'left' }}>
-              <Button
-                type="text"
-                icon={<IonIcon name="arrow-back-outline" />}
-                onClick={() => pushHistory('')}
-                style={{ margin: -12 }}
-              >
-                Back
-              </Button>
-            </Col>
-            <Col span={24}>
-              <Image src={REDEEM_IMG} preview={false} />
-            </Col>
-            <Col span={24}>
-              <Space direction="vertical" size={4}>
-                <Typography.Title level={3}>Redemption!</Typography.Title>
-                <Space size={4}>
-                  <Typography.Text type="secondary">Let's take</Typography.Text>{' '}
-                  <Typography.Title level={5} style={{ color: '#42E6EB' }}>
-                    {utils.undecimalize(
-                      BigInt(recipientData?.amount.toNumber() || 0),
-                      decimals,
-                    )}{' '}
-                    <MintSymbol
-                      mintAddress={distributor?.mint.toBase58() || ''}
-                    />
-                  </Typography.Title>
-                </Space>
-              </Space>
-            </Col>
-            <Col span={24}>
-              {!isValid ? (
-                <ButtonHome onBack={() => pushHistory('')} />
-              ) : (
-                <Button type="primary" onClick={onRedeem} loading={loading}>
-                  Redeem
+        <Spin spinning={loadingCard}>
+          <Card
+            style={{ minHeight: 430 }}
+            className="card-lightning"
+            bordered={false}
+          >
+            <Row gutter={[32, 32]} style={{ textAlign: 'center' }}>
+              <Col span={24} style={{ textAlign: 'left' }}>
+                <Button
+                  type="text"
+                  icon={<IonIcon name="arrow-back-outline" />}
+                  onClick={() => pushHistory('')}
+                  style={{ margin: -12 }}
+                >
+                  Back
                 </Button>
-              )}
-            </Col>
-          </Row>
-        </Card>
+              </Col>
+              <Col span={24}>
+                <Image src={REDEEM_IMG} preview={false} />
+              </Col>
+              <Col span={24}>
+                <Space direction="vertical" size={4}>
+                  <Typography.Title level={3}>Redemption!</Typography.Title>
+                  <Space size={4}>
+                    <Typography.Text type="secondary">
+                      Let's take
+                    </Typography.Text>{' '}
+                    <Typography.Title level={5} style={{ color: '#42E6EB' }}>
+                      {utils.undecimalize(
+                        BigInt(recipientData?.amount.toNumber() || 0),
+                        decimals,
+                      )}{' '}
+                      <MintSymbol
+                        mintAddress={distributor?.mint.toBase58() || ''}
+                      />
+                    </Typography.Title>
+                  </Space>
+                </Space>
+              </Col>
+              <Col span={24}>
+                {!isValid ? (
+                  <ButtonHome onBack={() => pushHistory('')} />
+                ) : (
+                  <Button type="primary" onClick={onRedeem} loading={loading}>
+                    Redeem
+                  </Button>
+                )}
+              </Col>
+            </Row>
+          </Card>
+        </Spin>
       </Col>
     </Row>
   )

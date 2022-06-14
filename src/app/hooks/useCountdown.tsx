3335aa5800e19428a9cceb5e6d8940327d723891
tuanle161099 <tuanle@descartes.network>
2022-06-14 09:18:31 +0700

    -

--- src/app/hooks/useCountdown.tsx
+++ src/app/hooks/useCountdown.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 
-export const DEFAULT_FIVE_MINUTE = 10 * 60
+export const DEFAULT_TEN_MINUTE = 10 * 60
 
 const useCountdown = () => {
-  const [timeRemaining, setTimeRemaining] = useState(DEFAULT_FIVE_MINUTE)
+  const [timeRemaining, setTimeRemaining] = useState(DEFAULT_TEN_MINUTE)
 
   useEffect(() => {
-    if (timeRemaining === 0) return setTimeRemaining(DEFAULT_FIVE_MINUTE)
+    if (timeRemaining === 0) return setTimeRemaining(DEFAULT_TEN_MINUTE)
     setTimeout(() => {
       setTimeRemaining(timeRemaining - 1)
     }, 1000)

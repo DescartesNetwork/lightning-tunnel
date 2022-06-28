0e9dc2437e22ff5249a5069021bf3d29c6d8ce57
minhcong <83624415+dmcong@users.noreply.github.com>
2022-06-28 17:55:37 +0700

    update fetch total balance

--- src/app/hooks/useTotalUSD.ts
+++ src/app/hooks/useTotalUSD.ts
@@ -1,88 +1,29 @@
-import { useCallback, useState } from 'react'
-import { useMint } from '@senhub/providers'
-
-import { fetchCGK } from 'shared/util'
-import TokenProvider from 'shared/tokenProvider'
-
-const DEFAULT_DATA = {
-  address: '',
-  icon: '',
-  name: 'TOKEN',
-  price: 0,
-  priceChange: 0,
-  rank: 0,
-  symbol: 'TOKEN',
-  totalVolume: 0,
-}
+import { useCallback, useEffect, useState } from 'react'
+import { useAccount } from '@senhub/providers'
+import { useCgk } from './useCgk'
 
 const useTotalUSD = () => {
-  const { tokenProvider } = useMint()
   const [totalUSD, setTotalUSD] = useState(0)
   const [loading, setLoading] = useState(true)
+  const { accounts } = useAccount()
+  const { getTotalBalance } = useCgk()
 
-  // const getMintDecimal = useCallback(
-  //   async (mintAddress: string) => {
-  //     try {
-  //       const decimals = await getDecimals(mintAddress)
-  //       return decimals
-  //     } catch (error) {
-  //       return 0
-  //     }
-  //   },
-  //   [getDecimals],
-  // )
+  const clcTotalUSD = useCallback(async () => {
+    try {
+      setLoading(true)
+      const totalUSD = await getTotalBalance(Object.values(accounts))
+      return setTotalUSD(totalUSD)
+    } catch (error) {
+    } finally {
+      setLoading(false)
+    }
+  }, [accounts, getTotalBalance])
 
-  const fetchCgkData = useCallback(
-    async (tokenProvider: TokenProvider, mintAddress: string) => {
-      try {
-        const token = await tokenProvider.findByAddress(mintAddress)
-        const ticket = token?.extensions?.coingeckoId
-        const cgkData = await fetchCGK(ticket)
-        return cgkData
-      } catch (error) {
-        return DEFAULT_DATA
-      }
-    },
-    [],
-  )
+  useEffect(() => {
+    clcTotalUSD()
+  }, [clcTotalUSD])
 
-  // const clcTotalUSD = useCallback(async () => {
-  //   try {
-  //     let totalUSD = 0
-  //     for (const account of Object.values(accounts)) {
-  //       const { mint, amount } = account
-  //       const decimals = await getMintDecimal(mint)
-  //       const cgkData = await fetchCgkData(tokenProvider, mint)
-  //       const tokenBalance = Number(utils.undecimalize(amount, decimals))
-  //       const usdBalance = tokenBalance * cgkData.price
-  //       totalUSD += usdBalance
-  //     }
-  //     return setTotalUSD(totalUSD)
-  //   } catch (error) {
-  //   } finally {
-  //     setLoading(false)
-  //   }
-  // }, [accounts, fetchCgkData, getMintDecimal, tokenProvider])
-
-  const calcUsdValue = useCallback(
-    async (address: string, amount: number) => {
-      try {
-        const cgkData = await fetchCgkData(tokenProvider, address)
-
-        return amount * cgkData.price
-      } catch (error) {
-      } finally {
-        setLoading(false)
-      }
-    },
-    [fetchCgkData, tokenProvider],
-  )
-
-  // useEffect(() => {
-  //   clcTotalUSD()
-  // }, [clcTotalUSD])
-
-  return { loading, totalUSD, calcUsdValue }
+  return { loading, totalUSD }
 }
 
 export default useTotalUSD
--- src/app/view/dashboard/hero/index.tsx
+++ src/app/view/dashboard/hero/index.tsx
@@ -1,13 +1,21 @@
-import { Col, Row } from 'antd'
-// import useTotalUSD from 'app/hooks/useBalance'
+import { Col, Row, Spin } from 'antd'
 import HeroCard from './heroCard'
 
+import useTotalUSD from 'app/hooks/useTotalUSD'
+
 const Hero = () => {
-  // const { totalUSD, loading } = useTotalUSD()
+  const { totalUSD, loading } = useTotalUSD()
+
   return (
     <Row gutter={[24, 24]}>
       <Col lg={8} md={12} xs={24}>
-        <HeroCard label="Total balance" icon="wallet-outline" value={200} />
+        <Spin spinning={loading}>
+          <HeroCard
+            label="Total balance"
+            icon="wallet-outline"
+            value={totalUSD}
+          />
+        </Spin>
       </Col>
       <Col lg={8} md={12} xs={24}>
         <HeroCard
